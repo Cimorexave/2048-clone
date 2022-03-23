a7b1d9ad163ad98ad15d3608c982cf355709a5c6
Cimorexave <sadeq.s502@gmail.com>
2022-03-23 14:44:54 +0430

    added controls

--- scripts/controls.js
+++ scripts/controls.js
@@ -1,1 +1,33 @@
-//
+//this script handles the controls of the game which include:
+// right key, left key, up key, down key
+
+const ArrowDown = () => {
+    console.log('Arrow Down key functions triggered...')
+}
+
+const ArrowUp = () => {
+    console.log('Arrow Up key functions triggered...')
+}
+
+const ArrowLeft = () => {
+    console.log('Arrow Left key functions triggered...')
+}
+
+const ArrowRight = () => {
+    console.log('Arrow Right key functions triggered...')
+}
+
+//first targeting the body and attaching the event to it so it can be triggered always
+
+document.querySelector("body").addEventListener('keydown', () => {
+    console.log('a key was pressed')
+    console.log(event.key) 
+    if (event.key == 'ArrowUp')
+        ArrowUp();
+    else if (event.key == 'ArrowDown')
+        ArrowDown();
+    else if (event.key == 'ArrowLeft')
+        ArrowLeft();
+    else if (event.key == 'ArrowRight')
+        ArrowRight();
+})
